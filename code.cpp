4882f02bd80298bbf83fe10d9d21c1176e777787
Eytan Brodsky <brodskye@flip3.engr.oregonstate.edu>
2018-04-12 16:59:49 -0700

    added tuple and made read file into a function

--- code.cpp
+++ code.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <fstream>
+#include <tuple>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+std::vector<int> read_file(std::string file){
+    std::vector<int> values;
+    std::ifstream infile(file);
+    int a, b, c;
+    char d,e;
+    while (infile >> a >> d >> b >> e >> c && d == e && e == ',')
+    {
+        values.push_back(a);
+        values.push_back(b);
+        values.push_back(c);
+    }
+    return values;
+}
 
 int main(int argc, char* argv[]) {
 
@@ -10,15 +26,6 @@ int main(int argc, char* argv[]) {
         std:cout << "Usage : < initial state file > < goal state file > < mode > < output file > " << endl;
         return 0;
     } else {
-        std::ifstream infile(argv[1]);
-        //std:: cout << argv[2] << " " << argv[3] << " " << argv[4] << " " << argv[5] << endl; 
-        int a, b, c;
-        char d,e;
-        while (infile >> a >> d >> b >> e >> c && d == e && e == ',')
-        {
-            std::cout << a << b << c << endl;
-        }
-
-        return 0;
+        std::vector<int> values = read_file(argv[1]);
     }
 }
