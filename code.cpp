80786322a239234e342c0e5546a895f8acbf9857
Pierre-Louis <sixdenip@oregonstate.edu>
2018-04-12 16:55:47 -0700

    Added arguments

--- code.cpp
+++ code.cpp
@@ -4,15 +4,21 @@
 using namespace std;
 
 
-int main() {
+int main(int argc, char* argv[]) {
 
+    if (argc < 5) {
+        std:cout << "Usage : < initial state file > < goal state file > < mode > < output file > " << endl;
+        return 0;
+    } else {
+        std::ifstream infile(argv[1]);
+        //std:: cout << argv[2] << " " << argv[3] << " " << argv[4] << " " << argv[5] << endl; 
+        int a, b, c;
+        char d,e;
+        while (infile >> a >> d >> b >> e >> c && d == e && e == ',')
+        {
+            std::cout << a << b << c << endl;
+        }
 
-    std::ifstream infile("start1.txt");
-    int a, b, c;
-    char d,e;
-    while (infile >> a >> d >> b >> e >> c && d == e && e == ',')
-    {
-        std::cout << a << b << c << endl;
+        return 0;
     }
-    return 0;
 }
